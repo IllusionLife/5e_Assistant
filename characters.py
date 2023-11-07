61276ea9601784beda0873f2d59bfd453367ca58
IllusionLife <illusion_life@outlook.com>
2023-11-07 22:31:36 +0200

    Modified main menu Added files for character creation and classes

---
+++ characters.py
@@ -0,0 +1,2 @@
+def create_character():
+    print("Success!")
--- main_menu.py
+++ main_menu.py
@@ -1,27 +1,56 @@
 import util as utl
+import characters as chr
 
-menu_items = {"1": ("Create character", "C"),
-              "2": ("View characters", "V"),
-              "3": ("Delete character", "D"),
-              "4": ("Add custom", "A"),
-              "0": ("Exit", "E")}
 
+def dummy():
+    """
+    Dummy function for menu items
+    """
+    print("Dummy!")
+    return
+
+
+
+menu_items = {"1": ("Create character", "C", chr.create_character),
+              "2": ("View characters", "V", dummy),
+              "3": ("Delete character", "D", dummy),
+              "4": ("Add custom", "A", dummy),
+              "0": ("Exit", "E", dummy)}
+"""
+Create a dictionary of a key (str) and a list. List must contain:
+  1. Title of functionality (str)
+  2. Access letter (can be left blank) (str)
+  3. Reference to function of the functionality (func)
+"""
 
 def get_menu_selection():
+    # Initiate variables
+    selection = None
     incorrect_selection = True
+
+    # Loop until valid selection
     while incorrect_selection:
+        # Print selections to the console
         print("Please select from the main menu:")
         for key in menu_items.keys():
             value = menu_items[key][0].replace(menu_items[key][1], f"({menu_items[key][1]})")
             print(f"{key}. {value}")
         selection = input("Selected:")
-        if (selection not in menu_items.keys() and
-                not any(selection in nkey for nval, nkey in list(menu_items.values()))):
+
+        # If access letter has been provided, convert it to key value
+        if any(selection in nkey for nval, nkey, nfunc in list(menu_items.values())):
+            for key, val in menu_items.items():
+                if val[1] == selection:
+                    selection = key
+
+        # Check if key exists
+        if selection not in menu_items.keys():
             utl.clear()
             print("Invalid choice.")
         else:
             incorrect_selection = False
-
+    # Call linked function
+    menu_items[selection][2]()
 
 
 def main_menu():
